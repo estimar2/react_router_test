71c42d30dd75eb3c208e88c7ce698f6d2302bd66
song <songsm5504@gmail.com>
2020-09-10 17:10:33 +0900

    router test

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Home, About, User } from "./pages";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/User">User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/User" component={User} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
